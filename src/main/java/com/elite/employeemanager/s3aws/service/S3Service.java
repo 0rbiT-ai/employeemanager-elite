21854afd67b00a59448147950717be408e6d33c1
Akhilesh <akhilesh6249@gmail.com>
2026-06-15 16:56:48 +0530

    implement aws s3 service with upload, download anddelete methods

--- src/main/java/com/elite/employeemanager/s3aws/service/S3Service.java
+++ src/main/java/com/elite/employeemanager/s3aws/service/S3Service.java
@@ -2,10 +2,14 @@ package com.elite.employeemanager.s3aws.service;
 
 import com.elite.employeemanager.s3aws.config.S3Properties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
@@ -18,11 +22,33 @@ public class S3Service {
     private final S3Properties s3Properties;
 
     public String uploadFile(MultipartFile file, String key) throws IOException {
+
+        if (file.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"File cannot be empty.");
+        }
+
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(s3Properties.getBucketName())
                 .key(key)
                 .build();
-        s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+        s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
         return key;
+    }
+
+    public byte[] downloadFile(String key){
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(s3Properties.getBucketName())
+                .key(key)
+                .build();
+
+        return s3Client.getObjectAsBytes(request).asByteArray();
+    }
+
+    public void deleteFile(String key){
+        DeleteObjectRequest request = DeleteObjectRequest.builder()
+                .bucket(s3Properties.getBucketName())
+                .key(key)
+                .build();
+        s3Client.deleteObject(request);
     }
 }
